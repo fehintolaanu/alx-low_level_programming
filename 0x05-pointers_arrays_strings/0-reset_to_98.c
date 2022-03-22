bd847813c553f898edf92df6930b25c4323db089
fehintolaanu <ibukunanu92@yahoo.com>
2022-03-22 11:33:22 -0700

    My 0 task commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+*include "main.h"
+
+/**
+ *reset_to_98 - updates an integer value to 98
+ *@n: A pointer to an integer that will be updated
+ *
+ *Return: void that means it is correct
+ */
+
+void reset_to_98(int *n)
+
+[
+ *n = 98;
+]
