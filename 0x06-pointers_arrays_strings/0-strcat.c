3f1478a14b2d1d45405c8cf76780f8d70aa0fa43
fehintolaanu <ibukunanu92@yahoo.com>
2022-03-24 02:00:34 -0700

    My task 0 commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *_strcat -concatenate two strings
 *@dest: A pointer to a character that will be changed
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strncat - concatenate two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: Value
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+int i, j;
+ i = 0;
